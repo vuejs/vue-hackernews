8bbfcbc2d341855e55ab175ec96eba40ed3c71ea
Evan You <yyx990803@gmail.com>
2015-10-19 10:57:07 -0400

    simplify build setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
+var vue = require('vue-loader')
 var webpack = require('webpack')
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: './static',
+    publicPath: '/static/',
     filename: 'build.js'
   },
   module: {
     loaders: [
-      { test: /\.vue$/, loader: 'vue' },
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      }
     ]
-  },
-  plugins: [
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -21,6 +29,9 @@ module.exports = {
       compress: {
         warnings: false
       }
-    })
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
   ]
+} else {
+  module.exports.devtool = '#source-map'
 }
